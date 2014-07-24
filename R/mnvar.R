b78b9dafc632d57367acfc2f304eeb1bce5f0e27
Jenna Krall <jkrall@jhsph.edu>
2014-07-24 13:14:43 -0400

    Add documentation for tlnise, data mnvar

---
+++ R/mnvar.R
@@ -0,0 +1,18 @@
+#' Mean and variances of sources for generating lognormal data
+#' 
+#' Note: on the log scale
+#' 
+#' \itemize{
+#' 		\item source1. mean, variance of first source
+#'     	\item source2. mean, variance of first source
+#'      \item source3. mean, variance of first source
+#'      \item source4. mean, variance of first source
+#'      \item source5. mean, variance of first source
+#'		...
+#'}
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A matrix with 2 rows and 5 variables
+#' @name mnvar
+NULL
