fc4868018c5bce17e08eeb9daac4e4fe0909a9ce
Jenna Krall <jkrall@jhsph.edu>
2014-07-25 20:30:29 -0400

    Fix warnings

--- R/health.R
+++ R/health.R
@@ -130,7 +130,7 @@ get.hosp <- function(dat, sources, lag, formu, outcome = "cardio", gv = "agecat"
                         data = merged, family = "quasipoisson",
                         offset = log(denom)), silent = T)
         options(warn = 1)
-        if(class(glm1[1]) == "try-error") {
+        if(suppressWarnings(class(glm1)) == "try-error") {
             glm1 <- try(glm(formula = eval(formUSE), 
                 data = merged, family = "quasipoisson",
                 offset = log(denom), control = list(epsilon = .00001)), 
@@ -138,7 +138,7 @@ get.hosp <- function(dat, sources, lag, formu, outcome = "cardio", gv = "agecat"
         }
         
         #save results
-        if(class(glm1[1]) != "try-error") {
+        if(suppressWarnings(class(glm1))  != "try-error") {
             out1 <- summary(glm1)$coef
             whS <- which(substr(rownames(out1), 1, 4) == "sour")
             out <- out1[whS, c(1, 2)]
