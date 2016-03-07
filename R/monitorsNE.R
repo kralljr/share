aaa81b01433c9ab52d9a54cce8a319cc2bc62920
kralljr <jenna.krall@gmail.com>
2016-03-07 11:37:27 -0500

    Add monitor location data for northeast

---
+++ R/monitorsNE.R
@@ -0,0 +1,14 @@
+#' Locations of 85 northeast US monitors used in SHARE study
+#' 
+#' \itemize{
+#' 		\item Latitude. Latitude in degrees (decimal form)
+#'     	\item Longitude. Longitude in degrees (decimal form)
+#'      \item monitor. Monitor name to match with constituent concentrations (consConc.rda)
+#'		...
+#'}
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A dataframe with 85 rows and 3 variables
+#' @name monitorsNE
+NULL
